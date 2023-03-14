cd79ac16797fba8ec19cfd10564c3f5583bf2d43
artir <artir.guri@mni.thm.de>
2023-03-14 17:59:08 +0100

    added a color.js for colors /and a renderbackgrounds for backgrounds and type backgrounds

---
+++ colors.js
@@ -0,0 +1,7 @@
+let colors = [
+    {
+        'grass': ['#49d0b0', '#61e1c9'],
+        'fire': ['#fc6c6d', '#f98c8d'],
+        'water': ['#76befe', '#85cafe']
+    }
+];
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let pokemon = [];
 let currentPokemon;
 
 async function loadPokemon(){
-for(let i = 0; i < 9; i++){
+for(let i = 0; i < 99; i++){
     let url = `https://pokeapi.co/api/v2/pokemon/${i+1}`;
     let response = await fetch(url);
     currentPokemon = await response.json();   
@@ -13,6 +13,8 @@ for(let i = 0; i < 9; i++){
 console.log(pokemon);
 
 renderAllPokemon();
+renderBackgroundsType();
+renderBackgrounds();
 }
 
 
@@ -26,12 +28,43 @@ function renderAllPokemon(){
         <h3 id="poke-${i}-name">${currentPokemon['name']}</h3>
         <div class="flex">
             <div class="types">
-                <div>type1</div>
-                <div>type2</div>
+                <div id="first-type-${i}">type1</div>
+                <div id="second-type-${i}">type2</div>
             </div>
             <img id="poke-${i}-img" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}" alt="">
         </div>
         </div>
         `;
+    }
+}
+
+
+function renderBackgrounds(){
+    for (let i = 0; i < pokemon.length; i++) {
+
+        if(pokemon[i]['types'][0]['type']['name'] == 'grass'){
+            document.getElementById(`poke-${i}`).style = `background-color: ${colors[0]['grass'][0]}`;
+        }else if(pokemon[i]['types'][0]['type']['name'] == 'fire'){
+            document.getElementById(`poke-${i}`).style = `background-color: ${colors[0]['fire'][0]}`;
+        }else if(pokemon[i]['types'][0]['type']['name'] == 'water'){
+            document.getElementById(`poke-${i}`).style = `background-color: ${colors[0]['water'][0]}`;
+        }
+    }
+
+}
+
+
+function renderBackgroundsType(){
+    for (let i = 0; i < pokemon.length; i++) {
+        if(pokemon[i]['types'][0]['type']['name'] == 'grass'){
+            document.getElementById(`first-type-${i}`).style = `background-color: ${colors[0]['grass'][1]}`;
+            document.getElementById(`second-type-${i}`).style = `background-color: ${colors[0]['grass'][1]}`;
+        }else if(pokemon[i]['types'][0]['type']['name'] == 'fire'){
+            document.getElementById(`first-type-${i}`).style = `background-color: ${colors[0]['fire'][1]}`;
+            document.getElementById(`second-type-${i}`).style = `background-color: ${colors[0]['fire'][1]}`;
+        }else if(pokemon[i]['types'][0]['type']['name'] == 'water'){
+            document.getElementById(`first-type-${i}`).style = `background-color: ${colors[0]['water'][1]}`;
+            document.getElementById(`second-type-${i}`).style = `background-color: ${colors[0]['water'][1]}`;
+        }
     }
 }
