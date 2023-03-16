9a897b457575bf5cf6aff0f972878ad0eadd8b5c
artir <artir.guri@mni.thm.de>
2023-03-16 17:49:09 +0100

    FIX: searchPokemon function works with backgrounds now

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function loadAllPokemon() {
 }
 
 
-async function saveallPokemonUrls() {
+async function loadAllPokemonUrls() {
     let response = await fetch(urlAllPokemon);
     response = await response.json();
 
@@ -20,10 +20,11 @@ async function saveallPokemonUrls() {
         allPokemonUrls.push(response.results[i].url);
     }
 }
-saveallPokemonUrls();
+loadAllPokemonUrls();
 
 
 async function loadPokemon(){
+    
     for(let i = 0; i < 30; i++){
         let url = allPokemonUrls[i];
         let response = await fetch(url);
@@ -35,61 +36,7 @@ async function loadPokemon(){
         renderBackgrounds(i, currentPokemon);
         renderBackgroundsType(i, currentPokemon);
     }
-    // renderAllPokemon();
-    // renderBackgroundsType();
 }
-
-
-function renderBackgroundsType(i, currentPokemon) {
-    let currentPokemonType = currentPokemon['types'][0]['type']['name'];
-    let currentFirstTypeId = document.getElementById(`first-type-${i}`);
-    let currentSecondTypeId = document.getElementById(`second-type-${i}`);
-    let currentBackgroundType = colors[0][currentPokemonType][1];
-
-        if (pokemon[i]['types'].length == 1) {
-            currentFirstTypeId.style = 'background-color:' + currentBackgroundType;
-        } else {
-            currentFirstTypeId.style = 'background-color:' + currentBackgroundType;
-            currentSecondTypeId.style = 'background-color:' + currentBackgroundType;
-        }
-}
-
-
-function renderBackgrounds(i, currentPokemon) {
-    let currentPokemonType = currentPokemon['types'][0]['type']['name'];
-    let currentBackground = colors[0][currentPokemonType][0];
-    let currentPokemonId = document.getElementById(`poke-${i}`);
-
-    currentPokemonId.style = 'background-color:' + currentBackground;
-}
-
-
-// function renderBackgrounds() {
-//     let currentPokemonType;
-//     let currentBackground;
-//     let currentPokemonId;
-//     for (let i = 0; i < pokemon.length; i++) {
-//         currentPokemonType = pokemon[i]['types'][0]['type']['name'];
-//         currentBackground = colors[0][currentPokemonType][0];
-//         currentPokemonId = document.getElementById(`poke-${i}`);
-
-//         currentPokemonId.style = 'background-color:' + currentBackground;
-//     }
-
-// }
-
-
-
-// function renderAllPokemon() {
-//     for (let i = 0; i < pokemon.length; i++) {
-//         const currentPokemon = pokemon[i];
-
-//         document.getElementById('all-pokemon').innerHTML += miniCardHtml(i, currentPokemon);
-
-//     }
-// }
-
-
 
 
 function searchPokemon() {
@@ -103,23 +50,35 @@ function searchPokemon() {
         let currentPokemon = pokemon[i];
         if (currentPokemon.name.toLowerCase().includes(search)) {
             pokemonShown.innerHTML += miniCardHtml(i, currentPokemon);
+            renderBackgrounds(i, currentPokemon);
+            renderBackgroundsType(i, currentPokemon);
         }
     }
 }
 
 
+function renderBackgrounds(i, currentPokemon) {
+    let currentPokemonType = currentPokemon['types'][0]['type']['name'];
+    let currentBackground = colors[0][currentPokemonType][0];
+    let currentPokemonId = document.getElementById(`poke-${i}`);
 
-// async function loadPokemon() {
-//     for (let i = 0; i < 30; i++) {
-//         let url = `https://pokeapi.co/api/v2/pokemon/${i + 1}`;
-//         let response = await fetch(url);
-//         currentPokemon = await response.json();
+    currentPokemonId.style = 'background-color:' + currentBackground;
+}
 
-//         pokemon.push(currentPokemon);
-//     }
-//     renderAll();
-// }
 
+function renderBackgroundsType(i, currentPokemon) {
+    let currentPokemonType = currentPokemon['types'][0]['type']['name'];
+    let currentFirstTypeId = document.getElementById(`first-type-${i}`);
+    let currentSecondTypeId = document.getElementById(`second-type-${i}`);
+    let currentBackgroundType = colors[0][currentPokemonType][1];
+
+        if (currentPokemon['types'].length == 1) {
+            currentFirstTypeId.style = 'background-color:' + currentBackgroundType;
+        } else {
+            currentFirstTypeId.style = 'background-color:' + currentBackgroundType;
+            currentSecondTypeId.style = 'background-color:' + currentBackgroundType;
+        }
+}
 
 
 
